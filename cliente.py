16e821eae8cf09ded90fea71760c2964953b232e
Newton Junior <newtoljunior@hotmail.com>
2017-10-27 06:57:08 -0200

    Update cliente.py
    correção de bugs e melhoria de performance no código

--- cliente.py
+++ cliente.py
@@ -5,6 +5,6 @@ with xmlrpc.client.ServerProxy("http://localhost:8000/") as proxy:
 	site = input("Digite o site: ")
 	siteCraw = 'http://'+site
 	palavra = input("Digite a palavra: ")
-	max_page = int(input("Digite a quantidade maxima de paginas: "))
+	#max_page = int(input("Digite a quantidade maxima de paginas: "))
 	print (siteCraw)
-	proxy.spider(siteCraw, palavra, max_page)
+	proxy.spider(siteCraw, palavra)
