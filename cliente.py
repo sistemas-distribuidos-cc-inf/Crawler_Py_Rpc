1e075afe69f0eaa4e01f4a30e87b8a5a06847c61
lipitos <fneves.si@gmail.com>
2017-10-31 00:48:40 -0200

    Add files via upload
    Implantado módulo para registro dos dados em um arquivo txt;
    Retorno de mensagem de sucesso ou falha no cliente;

--- cliente.py
+++ cliente.py
@@ -1,10 +1,16 @@
 import xmlrpc.client
+import sys
 
 with xmlrpc.client.ServerProxy("http://localhost:8000/") as proxy:
 
-	site = input("Digite o site: ")
-	siteCraw = 'http://'+site
-	palavra = input("Digite a palavra: ")
+	site = input("Please enter a website: ")
+	siteCraw = 'https://'+site
+	palavra = input("Enter a word: ")
 	#max_page = int(input("Digite a quantidade maxima de paginas: "))
-	print (siteCraw)
-	proxy.spider(siteCraw, palavra)
+	print ('Client conected')
+	#proxy.spider(siteCraw, palavra)
+	try:
+		proxy.spider(siteCraw, palavra)
+		print ('Search success!')
+	except:
+		print ('An error ocurred!')
